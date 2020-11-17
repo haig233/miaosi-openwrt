bb3a45ec902504742e26c3053a6ea3a9fc83cb49
本宫略萌 <3180435634@qq.com>
2020-11-17 11:04:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+svn co https://github.com/ophub/op/trunk/router/s905x3_phicomm-n1/install-program package/install-program
